dfdc43e3fe08000c985d4214a4c298906244ee56
claracantik28 <clarapratiwii28@gmail.com>
2023-12-18 11:41:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        primaryColor: Colors.white,        
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white))),
+      home: HomePage(),
+    );
+  }
+}
